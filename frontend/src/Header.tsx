11f552fee2ecefcc445fc4a9080974b2beb15c24
KStrzechowski <57865141+KStrzechowski@users.noreply.github.com>
2021-08-30 15:22:29 +0200

    Simple Questions

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { UserIcon } from './Icons';
+
 export const Header = () => (
   <div>
     <a href="./">Q & A</a>
     <input type="text" placeholder="Search..." />
     <a href="./sigin">
+      <UserIcon />
       <span>Sign In</span>
     </a>
   </div>
