77cb5374cd1cd06cc826b006be004c0ad1aab401
KStrzechowski <57865141+KStrzechowski@users.noreply.github.com>
2021-08-30 13:10:15 +0200

    Added project

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+export const Header = () => (
+  <div>
+    <a href="./">Q & A</a>
+    <input type="text" placeholder="Search..." />
+    <a href="./sigin">
+      <span>Sign In</span>
+    </a>
+  </div>
+);
